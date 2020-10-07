bf58c7cdeca4f9a904b0d1ee209d91518ec07330
Andrew Shulaev <ash.drone@gmail.com>
2020-10-07 01:28:11 +0100

    Laziest, barely functional UI

---
+++ src/main/kotlin/Server.kt
@@ -0,0 +1,20 @@
+import io.javalin.Javalin
+import java.io.File
+
+fun main() {
+    val app = Javalin.create().start(31337)
+    app.get("/") {
+        val index = File("static/index.html").readText()
+        it.html(index)
+    }
+    app.post("/run") {
+        val code = it.formParam("code")
+        if (code == null) {
+            it.result("no argument!")
+            return@post
+        }
+        val parsed = Parser(code).parse()
+        eval(parsed)
+        it.result("check server output")
+    }
+}
