7634163b892132bdf15e3fd66567e736ca56978d
Andrew Shulaev <ash.drone@gmail.com>
2021-01-16 01:05:02 +0000

    More progress on the script

--- devtools/add.py
+++ devtools/add.py
@@ -1,7 +1,9 @@
 # This is going to be a script that will be used to update a file in Git
 # repository easy with generated commit message.
 
+import git
 import os
+import sys
 from os import path
 
 
@@ -20,10 +22,24 @@ def find_git_repo(root):
 
     raise Exception(f'no git repository found from {root}')
 
+# Accepts absolute path for a file to be updated
+def update_file(file):
+    if not path.isfile(file):
+        raise Exception(f'{file} is not a path to file')
+
+    repo_dir = find_git_repo(path.dirname(file))
+    rel_file = path.relpath(file, repo_dir)
+
+    repo = git.Repo(repo_dir)
+    if len(repo.index.diff('HEAD')) > 0:
+        raise Exception(f'Repo {repo_dir} have staged files')
+
+    # repo.index.add([file])
+    message = f'Update {rel_file}'
 
 def main():
-    folder = find_git_repo(os.getcwd())
-    print(folder)
+    [_, file] = sys.argv
+    update_file(path.abspath(file))
 
 
 if __name__ == "__main__":
