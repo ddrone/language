2e5a171db30388781f2d7599c8b29ea6602321bb
Andrew Shulaev <ash.drone@gmail.com>
2021-02-09 00:13:47 +0000

    Implement the proper card review

--- riir/src/bin/review_cards.rs
+++ riir/src/bin/review_cards.rs
@@ -2,6 +2,8 @@ use note_rusty::*;
 use std::fs;
 use std::io;
 use std::io::Write;
+use chrono::prelude::*;
+use chrono::Duration;
 
 fn yes_no() -> bool {
     let mut input: String = String::new();
@@ -52,9 +54,25 @@ fn review_card(card: &CardData) -> bool {
 
 fn main() {
     let input = fs::read_to_string("cards.json").expect("oops");
-    let cards: Vec<Card> = serde_json::from_str(&input).expect("parse failed");
+    let mut cards: Vec<Card> = serde_json::from_str(&input).expect("parse failed");
 
-    for card in cards {
-        review_card(&card.data);
+    for card in &mut cards {
+        let time = Utc::now();
+        let days_to_review = 2_i64.pow(card.bucket as u32) - 1;
+        let next_review = card.last_reviewed.checked_add_signed(Duration::days(days_to_review as i64)).unwrap();
+        println!("Next review: {}", next_review);
+        if next_review < time {
+            if review_card(&card.data) {
+                card.bucket = card.bucket + 1;
+                println!("Moving to bucket {}", card.bucket);
+            } else {
+                card.bucket = 0;
+                println!("Moving to first bucket!");
+            }
+            card.last_reviewed = Utc::now();
+        }
     }
+
+    let json = serde_json::to_string(&cards).expect("serialize failed");
+    std::fs::write("cards.json", json).expect("write failed");
 }
