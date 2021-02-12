51dbb0aca9a3a3dab861cb051e7590ab214cd053
Andrew Shulaev <ash.drone@gmail.com>
2021-02-12 22:28:26 +0000

    Better review experience: shuffle cards, clear screen between cards

--- riir/src/review_cards.rs
+++ riir/src/review_cards.rs
@@ -2,6 +2,8 @@ use crate::cloze::{Cloze, ClozeChunk};
 use crate::*;
 use chrono::prelude::*;
 use chrono::Duration;
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::fs;
 use std::io;
 use std::io::Write;
@@ -55,12 +57,17 @@ fn review_card(card: &Cloze, close_id: usize) -> bool {
     yes_no()
 }
 
+pub fn clear_screen() {
+    print!("\x1B[2J\x1B[1;1H");
+}
+
 pub fn start_review(db: &str) {
     let input = fs::read_to_string(db).expect("oops");
     let mut cards: Vec<Card> = serde_json::from_str(&input).expect("parse failed");
+    let mut reviews: Vec<(usize, usize)> = Vec::new();
 
-    for card in &mut cards {
-        for (id, review) in (&mut card.review).into_iter().enumerate() {
+    for (i, card) in (&cards).into_iter().enumerate() {
+        for (j, review) in (&card.review).into_iter().enumerate() {
             let time = Utc::now();
             let days_to_review = 2_i64.pow(review.bucket as u32) - 1;
             let next_review = review
@@ -69,16 +76,26 @@ pub fn start_review(db: &str) {
                 .unwrap();
             println!("Next review: {}", next_review);
             if next_review < time {
-                if review_card(&card.data, id) {
-                    review.bucket = review.bucket + 1;
-                    println!("Moving to bucket {}", review.bucket);
-                } else {
-                    review.bucket = 0;
-                    println!("Moving to first bucket!");
-                }
-                review.last_reviewed = Utc::now();
+                reviews.push((i, j));
             }
         }
+    }
+
+    reviews.shuffle(&mut thread_rng());
+
+    for (i, j) in reviews {
+        let card = &mut cards[i];
+        let review = &mut card.review[j];
+        if review_card(&card.data, j) {
+            review.bucket = review.bucket + 1;
+            clear_screen();
+            println!("Moving to bucket {}", review.bucket);
+        } else {
+            review.bucket = 0;
+            clear_screen();
+            println!("Moving to first bucket!");
+        }
+        review.last_reviewed = Utc::now();
     }
 
     let json = serde_json::to_string(&cards).expect("serialize failed");
