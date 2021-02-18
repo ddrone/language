4711eee4cffc584cccb2e14c69c07ab43fbcaddd
Andrew Shulaev <ash.drone@gmail.com>
2021-02-18 23:00:04 +0000

    More complex data model for TODO

--- ui/src/list.ts
+++ ui/src/list.ts
@@ -1,13 +1,56 @@
-interface DataModel {
-    items: string[]
+interface TreeItem {
+    id: string,
+    text: string,
+    childen: TreeItem[],
+    // Ids of items that are dependencies of current item
+    dependencies: string[],
+    // If true, children do not implicitly take this item as dependency
+    group: boolean
 }
 
-function loadModel(): DataModel {
+class IdGenerator {
+    letters: string;
+    next_id: number;
+    next_number: number;
+
+    constructor() {
+        this.letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        this.next_id = 0;
+        this.next_number = 0;
+    }
+    
+    next(): string {
+        let result = this.letters[this.next_id] + this.next_number;
+        if (this.next_id >= this.letters.length) {
+            this.next_id = 0;
+            this.next_number += 1;
+        } else {
+            this.next_id += 1;
+        }
+        return result;
+    }
+
+    createItem(text: string): TreeItem {
+        return {
+            id: this.next(),
+            text: text,
+            childen: [],
+            dependencies: [],
+            group: false,
+        };
+    }
+}
+
+interface DataModel {
+    items: TreeItem[]
+}
+
+function loadModel(generator: IdGenerator): DataModel {
     let saved = window.localStorage.getItem('todo-graph');
     if (saved !== null) {
         return JSON.parse(saved);
     } else {
-        return {items: ["modify me"]};
+        return {items: [generator.createItem("modify me")]};
     }
 }
 
@@ -19,17 +62,18 @@ function initUI() {
     console.log("Initializing UI");
     let host = document.querySelector('#target');
     let list = document.createElement('ul');
-    let model = loadModel();
+    let generator = new IdGenerator();
+    let model = loadModel(generator);
 
     function updateItem(i: number, item: string) {
-        model.items[i] = item;
+        model.items[i].text = item;
         saveModel(model);
     }
 
     function createElement(i: number): HTMLInputElement {
         let item = document.createElement('li');
         let input = document.createElement('input');
-        input.value = model.items[i];
+        input.value = model.items[i].text;
         item.appendChild(input);
 
         input.addEventListener('focusout', () => {
@@ -48,7 +92,7 @@ function initUI() {
 
     newItem.addEventListener('click', () => {
         let i = model.items.length;
-        model.items.push('');
+        model.items.push(generator.createItem(''));
         let item = createElement(i);
         item.focus();
     });
