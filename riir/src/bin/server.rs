b215f88066c529aca1153f08d9bb19d046f9d013
Andrew Shulaev <ash.drone@gmail.com>
2021-02-25 19:12:41 +0000

    Implement root handler

--- riir/src/bin/server.rs
+++ riir/src/bin/server.rs
@@ -1,10 +1,32 @@
+use glob::glob;
 use std::process::Command;
 use warp::Filter;
 
+fn generate_link_list() -> String {
+    let mut result = String::new();
+    result.push_str("<ul>");
+
+    for entry in glob("**/*.md").unwrap() {
+        let file_path = entry.unwrap();
+        let file_name = file_path.to_str().unwrap();
+        result.push_str(&format!(
+            r#"
+            <li><a href="/read/{}">{}</a></li>
+        "#,
+            file_name, file_name
+        ));
+    }
+
+    result.push_str("</ul>");
+
+    result
+}
+
 #[tokio::main]
 async fn main() {
-    let root_handler = warp::path::end().map(|| format!("Root goes here"));
-    let read_handler = warp::path!("read" / String).map(|name| format!("File {} will go here", name));
+    let root_handler = warp::path::end().map(|| warp::reply::html(generate_link_list()));
+    let read_handler =
+        warp::path!("read" / String).map(|name| format!("File {} will go here", name));
 
     println!("Should start serving I think");
 
@@ -13,5 +35,7 @@ async fn main() {
         .spawn()
         .unwrap();
 
-    warp::serve(root_handler.or(read_handler)).run(([127, 0, 0, 1], 31337)).await;
+    warp::serve(root_handler.or(read_handler))
+        .run(([127, 0, 0, 1], 31337))
+        .await;
 }
