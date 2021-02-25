df8b5c5ed3ed9d42a15e9769389ab9485a2547e6
Andrew Shulaev <ash.drone@gmail.com>
2021-02-25 18:56:17 +0000

    More than one handler

--- riir/src/bin/server.rs
+++ riir/src/bin/server.rs
@@ -3,14 +3,15 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let server = warp::path!("read" / String).map(|name| format!("File {} will go here", name));
+    let root_handler = warp::path::end().map(|| format!("Root goes here"));
+    let read_handler = warp::path!("read" / String).map(|name| format!("File {} will go here", name));
 
     println!("Should start serving I think");
 
     Command::new("xdg-open")
-        .args(&["http://localhost:31337/read/note.md"])
+        .args(&["http://localhost:31337"])
         .spawn()
         .unwrap();
 
-    warp::serve(server).run(([127, 0, 0, 1], 31337)).await;
+    warp::serve(root_handler.or(read_handler)).run(([127, 0, 0, 1], 31337)).await;
 }
