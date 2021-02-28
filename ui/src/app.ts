cdaeb7fd38a8589bffdee11555f598dc3fac3241
Andrew Shulaev <ash.drone@gmail.com>
2021-02-28 08:50:36 +0000

    Display several cards in sequence

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -2,21 +2,49 @@ import * as m from 'mithril';
 
 var root = document.body;
 
-interface ReviewAttrs {
+interface Card {
     text: string;
     expected: string;
 }
 
-const Review: m.Component<ReviewAttrs> = {
-    view: function(vnode) {
-        return m("div", [
-            m("p", vnode.attrs.text),
-            m("textarea"),
-            m("button", "Check")
-        ]);
+interface ReviewAttrs {
+    cards: Card[];
+}
+
+interface ReviewState {
+    currentCard: number;
+}
+
+const Review: m.Component<ReviewAttrs, ReviewState> = {
+    oninit ({state}) {
+        state.currentCard = 0;
+    },
+    view: function({attrs, state}) {
+        if (state.currentCard < attrs.cards.length) {
+            let card = attrs.cards[state.currentCard];
+            return m("div", [
+                m("p", card.text),
+                m("textarea"),
+                m("button", {
+                    onclick: () => {
+                        state.currentCard++;
+                    }
+                }, "Check")
+            ]);
+        }
+        else {
+            return m("div", "All done!");
+        }
     }
 }
 
-m.render(root,
-    m(Review, {text: "This is a card text", expected: "answer"})
-);
+const Root: m.Component = {
+    view: function(_) {
+        return m(Review, {cards: [
+            {text: "This is a card text", expected: "answer"},
+            {text: "This is a second card", expected: "second answer"}
+        ]})
+    }
+}
+
+m.mount(root, Root);
