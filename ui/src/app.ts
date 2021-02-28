189af70760f052ddd7196a62614d6eab42ced76f
Andrew Shulaev <ash.drone@gmail.com>
2021-02-28 08:11:42 +0000

    Managed to write a simple component

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -2,4 +2,21 @@ import * as m from 'mithril';
 
 var root = document.body;
 
-m.render(root, 'Here be typed dragons');
+interface ReviewAttrs {
+    text: string;
+    expected: string;
+}
+
+const Review: m.Component<ReviewAttrs> = {
+    view: function(vnode) {
+        return m("div", [
+            m("p", vnode.attrs.text),
+            m("textarea"),
+            m("button", "Check")
+        ]);
+    }
+}
+
+m.render(root,
+    m(Review, {text: "This is a card text", expected: "answer"})
+);
