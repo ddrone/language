d619cd0dbecb7d0187913a8df2d6be62b67d71b6
Andrew Shulaev <ash.drone@gmail.com>
2021-03-06 11:06:21 +0000

    Playing with regex

---
+++ riir/src/lexer.rs
@@ -0,0 +1,23 @@
+use regex::Regex;
+
+pub fn to_chunks(source: &str) -> Vec<String> {
+    let regex = Regex::new(r"(?:\{\{.*?}})|(?:\$.*?\$)").unwrap();
+    regex.split(source).map(|s| s.to_string()).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::to_chunks;
+
+    #[test]
+    fn split() {
+        let result = to_chunks(r"There are {{deletions}} and formulas: $x^2 + y^2 = z^2$");
+        println!("{:?}", &result)
+    }
+
+    #[test]
+    fn multiple_entries() {
+        let result = to_chunks(r"$f: X \to Y$ with condition that $f(x_0) = y_0$");
+        println!("{:?}", &result)
+    }
+}
