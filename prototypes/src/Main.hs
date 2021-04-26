4ef2610ee668d40fbe790775128b7acddb8f0412
Andrew Shulaev <ash.drone@gmail.com>
2021-04-26 21:55:23 +0100

    Some datatypes

--- prototypes/src/Main.hs
+++ prototypes/src/Main.hs
@@ -1,5 +1,28 @@
 module Main where
 
+data Ty
+  = TySymbol
+  | Arr Ty Ty
+  deriving (Show)
+
+type Variable = String
+
+data Exp r
+  = Symbol String
+  | Var Variable
+  | Lam Variable Ty r
+  | App r r
+  deriving (Show)
+
+newtype Untyped = Untyped { getUntyped :: Exp Untyped }
+  deriving (Show)
+
+data Typed = Typed
+  { exprType :: Ty
+  , expr :: Exp Typed
+  }
+  deriving (Show)
+
 main :: IO ()
 main = do
   putStrLn "hello world"
