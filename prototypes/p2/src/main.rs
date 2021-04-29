70751c7f21a58434525447f274fad6903c642792
Andrew Shulaev <ash.drone@gmail.com>
2021-04-29 23:44:56 +0100

    Some types in Rust

--- prototypes/p2/src/main.rs
+++ prototypes/p2/src/main.rs
@@ -1,3 +1,27 @@
+enum Type {
+    Unit,
+    Arr(Ty, Ty)
+}
+type Ty = Box<Type>;
+
+enum ExpLayer<R> {
+    Unit,
+    Var(String),
+    Lam {
+        name: String,
+        ty: Type,
+        body: R
+    },
+    App(R, R)
+}
+
+type Id = u64;
+
+struct Exp {
+    id: Id,
+    layer: ExpLayer<Box<Exp>>
+}
+
 fn main() {
     println!("Hello, world!");
 }
