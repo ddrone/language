2f769c94b0367e28774f54b9611336ce4f13dbf3
Andrew Shulaev <ash.drone@gmail.com>
2021-04-30 00:05:20 +0100

    Write a traversal for expressions

--- prototypes/p2/src/main.rs
+++ prototypes/p2/src/main.rs
@@ -1,9 +1,13 @@
+use std::borrow::BorrowMut;
+
+#[derive(Debug)]
 enum Type {
     Unit,
     Arr(Ty, Ty)
 }
 type Ty = Box<Type>;
 
+#[derive(Debug)]
 enum ExpLayer<R> {
     Unit,
     Var(String),
@@ -17,9 +21,35 @@ enum ExpLayer<R> {
 
 type Id = u64;
 
+#[derive(Debug)]
 struct Exp {
     id: Id,
     layer: ExpLayer<Box<Exp>>
+}
+
+fn traverse<F>(exp: &mut Exp, f: &mut F) where
+    F: FnMut(&mut Exp) -> ()
+{
+    f(exp);
+    match &mut exp.layer {
+        ExpLayer::Unit => {}
+        ExpLayer::Var(_) => {}
+        ExpLayer::Lam { ref mut body, .. } => {
+            traverse(body.borrow_mut(), f)
+        }
+        ExpLayer::App(ref mut fun, ref mut arg) => {
+            traverse(fun.borrow_mut(), f);
+            traverse(arg.borrow_mut(), f);
+        }
+    }
+}
+
+fn assign_unique_ids(exp: &mut Exp) {
+    let mut id: u64 = 0;
+    traverse(exp, &mut |e: &mut Exp| {
+        e.id = id;
+        id = id + 1;
+    });
 }
 
 fn main() {
