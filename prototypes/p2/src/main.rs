f01e0eabd85c45439ac7ce9af23f67b5d065a87c
Andrew Shulaev <ash.drone@gmail.com>
2021-04-30 10:19:39 +0100

    Got the basic typechecker running

--- prototypes/p2/src/main.rs
+++ prototypes/p2/src/main.rs
@@ -1,6 +1,6 @@
-use std::borrow::BorrowMut;
+use std::borrow::{Borrow, BorrowMut};
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Clone)]
 enum Type {
     Uint,
     Arr(Ty, Ty),
@@ -11,7 +11,7 @@ type Ty = Box<Type>;
 enum ExpLayer<R> {
     Uint(u64),
     Var(String),
-    Lam { name: String, ty: Type, body: R },
+    Lam { name: String, ty: Ty, body: R },
     App(R, R),
 }
 
@@ -30,6 +30,20 @@ where
     Exp {
         id: 0,
         layer: ExpLayer::Var(s.to_string()),
+    }
+}
+
+fn lam<S>(name: S, ty: Ty, body: Exp) -> Exp
+where
+    S: ToString,
+{
+    Exp {
+        id: 0,
+        layer: ExpLayer::Lam {
+            name: name.to_string(),
+            ty,
+            body: Box::new(body),
+        },
     }
 }
 
@@ -64,9 +78,52 @@ fn assign_unique_ids(exp: &mut Exp) {
     });
 }
 
+// TODO: figure out an idiomatic way to do this more performantly in Rust
+fn lookup(env: &Vec<(String, Ty)>, key: String) -> Result<Ty, String> {
+    for p in env.iter().rev() {
+        if p.0.eq(&key) {
+            return Ok(p.1.clone());
+        }
+    }
+    return Err(key + " not found");
+}
+
+fn typecheck(env: &mut Vec<(String, Ty)>, exp: &Exp) -> Result<Ty, String> {
+    match &exp.layer {
+        ExpLayer::Uint(_) => Ok(Box::new(Type::Uint)),
+        ExpLayer::Var(ref v) => lookup(&env, v.clone()),
+        ExpLayer::Lam {
+            ref name,
+            ref ty,
+            ref body,
+        } => {
+            env.push((name.clone(), ty.clone()));
+            let body_ty = typecheck(env, body)?;
+            let result_ty = Box::new(Type::Arr(ty.clone(), body_ty));
+            Ok(result_ty)
+        }
+        ExpLayer::App(ref fun, ref arg) => {
+            let fun_ty = typecheck(env, fun)?;
+            let arg_ty = typecheck(env, arg)?;
+            match *fun_ty {
+                Type::Arr(fun_arg, fun_res) => {
+                    if fun_arg.eq(&arg_ty) {
+                        Ok(fun_res)
+                    } else {
+                        Err("function applied to wrong argument".to_string())
+                    }
+                }
+                _ => Err("trying to apply non-function".to_string()),
+            }
+        }
+    }
+}
+
 fn main() {
-    let mut test = app(var("f"), var("x"));
+    let mut test = lam("x", Box::new(Type::Uint), var("x"));
     println!("{:?}", &test);
     assign_unique_ids(&mut test);
     println!("{:?}", &test);
+    let ty = typecheck(&mut Vec::new(), &test);
+    println!("{:?}", &ty);
 }
