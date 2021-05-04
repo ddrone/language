ea7389d9cc06c22a0104803e2c0b70250f01cfac
Andrew Shulaev <ash.drone@gmail.com>
2021-05-04 01:20:46 +0100

    Add an ability to specify topic in writing script

--- devtools/write.py
+++ devtools/write.py
@@ -1,5 +1,14 @@
+import os
+import sys
 from datetime import datetime
 from subprocess import run
 
 name = datetime.utcnow().strftime("%Y%m%d-%H%M%S.md")
-run(['code', name])
+try:
+    # -t stands for "topic"
+    topic_index = sys.argv.index('-t')
+    path = os.path.join(sys.argv[topic_index + 1], name)
+except:
+    path = name
+
+run(['code', path])
